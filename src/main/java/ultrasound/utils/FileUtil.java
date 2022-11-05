5c3043a885ee35dd6f42cbf417bdf2eab63bcf00
= <maciej.sadowski@outlook.com>
2022-11-05 10:47:41 +0100

    New FileUtil feature: saving data to .csv file

--- src/main/java/ultrasound/utils/FileUtil.java
+++ src/main/java/ultrasound/utils/FileUtil.java
@@ -1,8 +1,14 @@
 package ultrasound.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -108,4 +114,43 @@ public final class FileUtil {
 
 		return true;
 	}
+	
+	public static boolean saveToCsvFile(String name, short[] data) {
+
+		if (null == name || null == data) {
+			return false;
+		}
+		File myFile = new File(name + ".csv");
+		
+		try (FileWriter writer = new FileWriter(myFile,StandardCharsets.UTF_8)) {
+
+			int len = data.length;
+			for (int j = 0; j < len; j++) {
+				writer.write(data[j] + "," + "");
+			}
+
+		} catch (Exception ex) {
+			return false;
+		}
+
+		return true;
+	}
+	
+	public static List<String> readDataFromCsvFile(File file) {
+		List<String> vals = new ArrayList<>();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(file,  StandardCharsets.UTF_8))) {
+			String line;
+			while((line = br.readLine()) != null) {
+				String[] values = line.split(",");
+				vals.addAll(Arrays.asList(values));
+			}
+		} catch (IOException e) {
+			System.err.println("File not found!");
+		}
+		
+		return vals;
+		
+	}
+	
 }
